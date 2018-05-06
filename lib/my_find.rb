247744f1fb69c7f55e276eceda8cb6e677c7e895
Tuong Van Nguyen <tvan262@yahoo.com>
2018-05-06 01:07:06 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -7,3 +7,13 @@ def my_find(collection)
    i = i + 1                                         #if yield(collection[i])
  end                                                 #return collection[i]
 end
+
+#    def my_find(collection) 
+#      i = 0
+#   while i < collection.length
+#    if yield(collection[i])
+#      return collection[i]
+#    end
+#    i = i + 1
+#  end
+# end
